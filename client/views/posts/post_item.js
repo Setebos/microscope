1241619f5b27652e820b798ab7bed59e0ce2a352
IUT <user01@user01-virtual-machine.(none)>
2014-02-20 17:39:54 +0100

    fin du tuto

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -7,7 +7,43 @@ Template.postItem.helpers({
 		a.href = this.url;
 		return a.hostname;
 	},
-	commentsCount: function() {
-		return Comments.find({postId: this._id}).count();
+	upvotedClass: function() {
+		var userId = Meteor.userId();
+		if(userId && !_.include(this.upvoters, userId)) {
+			return 'btn-primary upvotable';
+		} else {
+			return 'disabled';
+		}
+	}
+});
+
+Template.postItem.rendered = function(){
+	// animate post from previous position to new position
+	var instance = this;
+	var rank = instance.data._rank;
+	var $this = $(this.firstNode);
+	var postHeight = 80;
+	var newPosition = rank * postHeight;
+	// if element has a currentPosition (i.e. it's not the first ever render)
+	if (typeof(instance.currentPosition) !== 'undefined') {
+		var previousPosition = instance.currentPosition;
+		// calculate difference between old position and new position and send element there
+		var delta = previousPosition - newPosition;
+		$this.css("top", delta + "px");
+	} else {
+		$this.addClass("invisible");
+	}
+	// let it draw in the old position, then..
+	Meteor.defer(function() {
+		instance.currentPosition = newPosition;
+		// bring element back to its new original position
+		$this.css("top", "0px").removeClass("invisible");
+	});
+};
+
+Template.postItem.events({
+	'click .upvotable': function(e) {
+		e.preventDefault();
+		Meteor.call('upvote', this._id);
 	}
 });
--- server/fixtures.js
+++ server/fixtures.js
@@ -13,7 +13,9 @@ if (Posts.find().count() === 0) {
 		userId: sacha._id,
 		author: sacha.profile.name,
 		url: 'http://sachagreif.com/introducing-telescope/',
-		submitted: now - 7 * 3600 * 1000
+		submitted: now - 7 * 3600 * 1000,
+		commentsCount: 2,
+		upvoters: [], votes: 0
 	});
 	Comments.insert({
 		postId: telescopeId,
@@ -34,13 +36,28 @@ if (Posts.find().count() === 0) {
 		userId: tom._id,
 		author: tom.profile.name,
 		url: 'http://meteor.com',
-		submitted: now - 10 * 3600 * 1000
+		submitted: now - 10 * 3600 * 1000,
+		commentsCount: 0,
+		upvoters: [], votes: 0
 	});
 	Posts.insert({
 		title: 'The Meteor Book',
 		userId: tom._id,
 		author: tom.profile.name,
 		url: 'http://themeteorbook.com',
-		submitted: now - 12 * 3600 * 1000
+		submitted: now - 12 * 3600 * 1000,
+		commentsCount: 0,
+		upvoters: [], votes: 0
 	});
+	for (var i = 0; i < 10; i++) {
+		Posts.insert({
+			title: 'Test post #' + i,
+			author: sacha.profile.name,
+			userId: sacha._id,
+			url: 'http://google.com/?q=test-' + i,
+			submitted: now - i * 3600 * 1000,
+			commentsCount: 0,
+			upvoters: [], votes: 0
+		});
+	}
 }
