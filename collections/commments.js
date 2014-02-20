bb2b8a19fb2befc0975352482c5ab28c18ed4556
Setebos <moffjerjerrod@gmail.com>
2014-02-20 23:40:15 +0100

    pas grand chose

--- collections/commments.js
+++ collections/commments.js
@@ -4,17 +4,25 @@ Meteor.methods({
 	comment: function(commentAttributes) {
 		var user = Meteor.user();
 		var post = Posts.findOne(commentAttributes.postId);
-		if (!user)
+
+		if (!user) {
 			throw new Meteor.Error(401, "You need to login to make comments");
-		if (!commentAttributes.body)
+		}
+
+		if (!commentAttributes.body) {
 			throw new Meteor.Error(422, 'Please write some content');
-		if (!post)
+		}
+
+		if (!post) {
 			throw new Meteor.Error(422, 'You must comment on a post');
+		}
+
 		comment = _.extend(_.pick(commentAttributes, 'postId', 'body'), {
 			userId: user._id,
 			author: user.username,
 			submitted: new Date().getTime()
 		});
+		
 		Posts.update(comment.postId, {$inc: {commentsCount: 1}});
 		comment._id = Comments.insert(comment);
 		createCommentNotification(comment);
